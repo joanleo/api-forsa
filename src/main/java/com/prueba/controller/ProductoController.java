ced28e7453473ead4c28d92b97e970d451a46831
Valencia-Metrolink <avalencia@metrolinkeu.com>
2022-05-09 13:52:27 -0500

    Se actualiza el metodo http para actualizar el estado de verificado de producto cambia de PUT a PATCH

--- src/main/java/com/prueba/controller/ProductoController.java
+++ src/main/java/com/prueba/controller/ProductoController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -55,7 +56,7 @@ public class ProductoController {
 		return ResponseEntity.ok(productoService.getProducto(id));
 	}
 	
-	@PutMapping("/{id}")
+	@PatchMapping("/{id}")
 	public ResponseEntity<ProductoDTO> verify(@PathVariable(name = "id") Long id){
 		
 		return new ResponseEntity<ProductoDTO>(productoService.receive(id), HttpStatus.ACCEPTED);
