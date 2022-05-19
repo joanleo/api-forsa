51822448de1a3928f7bc9eda54280c1fbe31dbd8
Valencia-Metrolink <avalencia@metrolinkeu.com>
2022-05-19 12:00:01 -0500

    Cors config allowedAllMethods

--- src/main/java/com/prueba/security/CorsConfig.java
+++ src/main/java/com/prueba/security/CorsConfig.java
@@ -2,6 +2,7 @@ package com.prueba.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -16,7 +17,8 @@ public class CorsConfig{
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedOrigins("*")
+                .allowedMethods("*");
             }
         };
     }
