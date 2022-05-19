4135d6c2354787b8099aa5c14a2c32a6aad4a16f
Valencia-Metrolink <avalencia@metrolinkeu.com>
2022-05-19 09:37:30 -0500

    sin cambios

--- src/main/java/com/prueba/security/CorsConfig.java
+++ src/main/java/com/prueba/security/CorsConfig.java
@@ -3,18 +3,21 @@ package com.prueba.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SuppressWarnings("deprecation")
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer{
+public class CorsConfig{
 	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-	    registry.addMapping("/**")
-	    .allowedOrigins("*");
-	}
+	@Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                .allowedOrigins("*");
+            }
+        };
+    }
 }
