6c9c4695c09dfa1d4120dfbcc8d596e857f17165
jleonocampo@gmail.com <avalencia@metrolinkeu.com>
2022-08-08 18:45:43 -0500

    Se modifica entidad Trazabilidad

--- src/main/java/com/prueba/entity/Trazabilidad.java
+++ src/main/java/com/prueba/entity/Trazabilidad.java
@@ -4,21 +4,29 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import com.prueba.security.entity.Usuario;
 
 @Entity
 @Table(name = "mov_trazas")
 public class Trazabilidad {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+    		generator = "seq_traza")
+	@SequenceGenerator(name = "seq_traza", allocationSize = 10)
 	@Column(name = "vcid_traza", length = 6)
 	private Long id;
 	
@@ -32,52 +40,111 @@ public class Trazabilidad {
 		fecha = new Date();
 	}
 	
-	@Column(name = "vcdescripcion")
-	private String descripcion;
+	@Column(name = "vcoperacion")
+	private String operacion;
 	
-	@OneToOne(targetEntity = TipoMov.class, cascade = javax.persistence.CascadeType.ALL)
-	private TipoMov tipoMov;
-	
-	@OneToOne(targetEntity = Producto.class, cascade = javax.persistence.CascadeType.ALL)
-	private Producto producto;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "nidusuario", referencedColumnName = "nidusuario")
+	private Usuario usuario;
 
+	@Column(name = "vcruta")
+	private String ruta;
+	
+	/**
+	 * 
+	 */
+	public Trazabilidad() {
+		super();
+	}
+
+	/**
+	 * @param operacion
+	 * @param usuario
+	 * @param ruta
+	 */
+	public Trazabilidad(String operacion, Usuario usuario, String ruta) {
+		super();
+		this.operacion = operacion;
+		this.usuario = usuario;
+		this.ruta = ruta;
+	}
+
+	/**
+	 * @return
+	 */
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * @param id
+	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return
+	 */
 	public Date getFecha() {
 		return fecha;
 	}
 
+	/**
+	 * @param fecha
+	 */
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
 
-	public String getDescripcion() {
-		return descripcion;
+	/**
+	 * @return
+	 */
+	public String getOperacion() {
+		return operacion;
 	}
 
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
+	/**
+	 * @param operacion
+	 */
+	public void setOperacion(String operacion) {
+		this.operacion = operacion;
 	}
 
-	public TipoMov getTipoMov() {
-		return tipoMov;
+	/**
+	 * @return
+	 */
+	public Usuario getUsuario() {
+		return usuario;
 	}
 
-	public void setTipoMov(TipoMov tipoMov) {
-		this.tipoMov = tipoMov;
+	/**
+	 * @param usuario
+	 */
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
-	public Producto getProducto() {
-		return producto;
+	/**
+	 * @return
+	 */
+	public String getRuta() {
+		return ruta;
 	}
 
-	public void setProducto(Producto producto) {
-		this.producto = producto;
+	/**
+	 * @param ruta
+	 */
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
+	}
+
+	/**
+	 *
+	 */
+	@Override
+	public String toString() {
+		return "Trazabilidad [id=" + id + ", fecha=" + fecha + ", operacion=" + operacion + ", usuario=" + usuario
+				+ ", ruta=" + ruta + "]";
 	}
 }
