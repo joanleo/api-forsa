eb954cfc18079e51ab48faaa35c12909d1f329c0
jleonocampo@gmail.com <avalencia@metrolinkeu.com>
2022-09-12 18:17:42 -0500

    Se añade JpaSpecificationExecutor

--- src/main/java/com/prueba/repository/PermisoRepository.java
+++ src/main/java/com/prueba/repository/PermisoRepository.java
@@ -1,10 +1,11 @@
 package com.prueba.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.prueba.entity.Permiso;
 
-public interface PermisoRepository extends JpaRepository<Permiso, Long> {
+public interface PermisoRepository extends JpaRepository<Permiso, Long>, JpaSpecificationExecutor<Permiso> {
 
 	Permiso findByUrlAndNombre(String ruta, String nombre);
 
